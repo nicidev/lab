f81481dd14aff18fc31556dc465db7e8cb56b97c
Nicole Heinze <nicole.heinze@accso.de>
2024-08-30 18:11:07 +0200

    Creating multiple API endpoints

--- Quarkus for beginners/rest-book/src/main/java/dev/nici/BookResource.java
+++ Quarkus for beginners/rest-book/src/main/java/dev/nici/BookResource.java
@@ -1,16 +1,37 @@
 package dev.nici;
 
+import java.util.List;
+import java.util.Optional;
+
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
+@Path("/api/books")
+@Produces(MediaType.APPLICATION_JSON)
 public class BookResource {
 
     @GET
+    public List<Book> getAllBooks() {
+        return List.of(
+                new Book(1, "test", "Nici", 2024, "Horror"),
+                new Book(2, "Buchibuch", "Nici", 2024, "Kinderbuch"),
+                new Book(3, "Bibel", "Nici", 2024, "Liebesroman")
+                );
+    }
+
+    @GET
+    @Path("/count")
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    public int countAllBooks(){
+        return getAllBooks().size();
+    }
+
+    @GET
+    @Path("{id}")
+    public Optional<Book> getBook(@PathParam("id") int id){
+        return getAllBooks().stream().filter(book -> book.id == id).findFirst();
     }
 }
